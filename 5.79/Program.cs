9d8a676c2a4bdc651cd0ee6bfa182bd754816ad5
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:06:38 +0300

    Add files via upload

---
+++ 5.79/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _5._79
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение x: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите значение y: ");
+            int b = Int32.Parse(Console.ReadLine());
+            int c = 0;
+            for (int i = 0; i < b; i++)
+            {
+                c += a;
+            }
+            Console.WriteLine($"Результат умножения через функцию: {c}");
+
+            int v = u(a, b);
+            Console.WriteLine($"Результат умножения через рекурсию: {v}");
+        }
+
+        static int u(int x, int y)
+        {
+            if (x == 0 || y == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return x + u(x, y - 1);
+            }
+        }
+    }
+}
